a3c4134a3e3b291cda299749e521eb1a66ddb280
iuliamoldovan <iulia.moldovan@localsearch.ch>
2023-03-20 13:51:50 +0200

    Remove deprecated version of security configuration

--- src/main/java/ch/poc/configserver/security/SecurityConfiguration.java
+++ src/main/java/ch/poc/configserver/security/SecurityConfiguration.java
@@ -1,25 +1,28 @@
 package ch.poc.configserver.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter
+public class SecurityConfiguration
 {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
 		http
 			.csrf().disable()
 			.cors().disable()
-			.authorizeRequests()
-			.antMatchers("/.well-known/**").permitAll()
-			.anyRequest().authenticated()
-			.and()
+			.authorizeHttpRequests((authz) ->
+				authz.antMatchers("/.well-known/**").permitAll()
+					.anyRequest().authenticated()
+			)
 			.httpBasic()
 			.and()
 			.formLogin()
 			.disable();
+		return http.build();
 	}
 }
